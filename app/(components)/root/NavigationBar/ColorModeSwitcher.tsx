380d23fbf68f90d81d353dca2b255f0871dabda5
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-12-18 19:24:45 +0100

    refactor: simplified color-mode updates
    Simplified the code base that changes the current color mode, when the ColorSwitcher component is pressed. This restructuring fixed the static icon, that indicates the current color-mode, since the icon change was depended on the color-mode cookie.

--- app/(components)/root/NavigationBar/ColorModeSwitcher.tsx
+++ app/(components)/root/NavigationBar/ColorModeSwitcher.tsx
@@ -6,48 +6,39 @@ import { Transition } from '@headlessui/react'
 import { twMerge } from 'tailwind-merge'
 import { GlobalColorSettings } from '@/lib/root/ColorSettings'
 
+function updateColorMode(mode: 'dark' | 'light', cookiesAllowed: boolean) {
+  document.documentElement.classList.toggle('dark')
+  document.documentElement.classList.toggle(GlobalColorSettings.lightBackground) //? change document background-color
+  document.documentElement.classList.toggle(GlobalColorSettings.raw_darkBackground) //? change document background-color
+
+  if (cookiesAllowed) updateCookies(mode)
+  else console.log('Essential cookies are denied, color-mode prefence has not been saved!')
+}
+
+function updateCookies(mode: 'dark' | 'light') {
+  setCookie(null, 'colorMode', mode, {
+    maxAge: 24 * 60 * 60 * 300, // 300 days
+    path: '/',
+    secure: false,
+    sameSite: true,
+  })
+}
+
 export default function ColorModeSwitcher({ className }: { className?: string }) {
   const [mode, setMode] = useState<'light' | 'dark' | undefined>()
-  const cookiePermission = !!parseCookies()?.essentialCookies
+  const cookiePermission = () => !!parseCookies()?.essentialCookies
 
-  const current = () => parseCookies()?.colorMode
-  const toggleMode = () => {
-    const updateCookies = (mode: 'dark' | 'light') => {
-      if (cookiePermission) {
-        setCookie(null, 'colorMode', mode, {
-          maxAge: 24 * 60 * 60 * 300, // 300 days
-          path: '/',
-          secure: false,
-          sameSite: true,
-        })
-      } else console.log(`Essential cookies were denied, color-mode preference won't be saved.`)
+  const handleToggle = () => {
+    const newMode = mode === 'light' ? 'dark' : 'light'
 
-      document.documentElement.classList.toggle('dark')
-      document.documentElement.classList.toggle(GlobalColorSettings.lightBackground)
-      document.documentElement.classList.toggle(GlobalColorSettings.raw_darkBackground)
-    }
-
-    //? When the user first visits the site, we want to set the color mode to their preference.
-    if (!current()) {
-      const userPreference = window?.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-      updateCookies(userPreference)
-      setMode(userPreference)
-      return
-    }
-
-    const newMode = current() === 'dark' ? 'light' : 'dark'
-    updateCookies(newMode)
     setMode(newMode)
+    updateColorMode(newMode, cookiePermission())
   }
 
   useEffect(() => {
-    if (!current()) {
-      console.log('Welcome, your color-preference has been saved.')
-      toggleMode()
-      return
-    }
-
-    setMode(current() === 'light' ? 'light' : 'dark')
+    const preference = parseCookies()?.colorMode ?? window?.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    setMode(preference) //? Set the initial color-preference
+    updateColorMode(preference, cookiePermission())
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
@@ -55,11 +46,11 @@ export default function ColorModeSwitcher({ className }: { className?: string })
   return (
     <div className={twMerge('h-6 w-6 hover:cursor-pointer', className)}>
       <Transition show={mode === 'light'} enter='transition duration-300' enterFrom='rotate-45 opacity-50' enterTo='rotate-0 opacity-100'>
-        {mode === 'light' && <SunIcon onClick={toggleMode} />}
+        {mode === 'light' && <SunIcon onClick={handleToggle} />}
       </Transition>
 
       <Transition show={mode === 'dark'} enter='transition duration-300' enterFrom='-rotate-45 opacity-50' enterTo='rotate-45 opacity-100'>
-        {mode === 'dark' && <MoonIcon onClick={toggleMode} />}
+        {mode === 'dark' && <MoonIcon onClick={handleToggle} />}
       </Transition>
     </div>
   )
