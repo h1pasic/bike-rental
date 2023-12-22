6dc988497425d20e64ae4f6e53b791ac91b23a6f
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-12-22 13:55:12 +0100

    refactor: removed SidebarProps interface
    The interface was removed as it became redundant, since it only contained the property 'elements'.

--- app/(components)/root/NavigationBar/SideBar.tsx
+++ app/(components)/root/NavigationBar/SideBar.tsx
@@ -1,45 +1,34 @@
 import { SideElementProps } from '@/app/(components)/root/NavigationBar/SideElement'
-import { StaticImageData } from 'next/image'
 import DesktopSidebar from '@/app/(components)/root/NavigationBar/DesktopSidebar'
 import MobileSidebar from '@/app/(components)/root/NavigationBar/MobileSidebar'
 import getSessionData from '@/lib/Shared/getSessionData'
 
-export interface SidebarProps {
-  elements: SideElementProps[]
-  user?: {
-    name: string
-    image: StaticImageData | string
-  }
-}
-
 export default async function SideBar() {
   const { data } = await getSessionData()
 
-  const SidebarProps: SidebarProps = {
-    elements: [
-      { name: 'Home', icon: 'HomeIcon', href: '/' },
-      {
-        name: 'Browse',
-        icon: 'FolderIcon',
-        subitems: [
-          {
-            name: 'Rental Shops',
-            href: '/stations/',
-            icon: 'BuildingStoreFrontIcon',
-            badge: 15,
-          },
-          {
-            name: 'Available Bikes',
-            href: '/bikes/',
-            icon: 'ShoppingBagIcon',
-            badge: 45,
-          },
-        ],
-      },
-      { name: 'Book Tickets', icon: 'CursorArrowRaysIcon', href: '/tickets' },
-    ],
-  }
-  const ManagementElements: SideElementProps[] = [
+  const elements: SideElementProps[] = [
+    { name: 'Home', icon: 'HomeIcon', href: '/' },
+    {
+      name: 'Browse',
+      icon: 'FolderIcon',
+      subitems: [
+        {
+          name: 'Rental Shops',
+          href: '/stations/',
+          icon: 'BuildingStoreFrontIcon',
+          badge: 15,
+        },
+        {
+          name: 'Available Bikes',
+          href: '/bikes/',
+          icon: 'ShoppingBagIcon',
+          badge: 45,
+        },
+      ],
+    },
+    { name: 'Book Tickets', icon: 'CursorArrowRaysIcon', href: '/tickets' },
+  ]
+  const managementElements: SideElementProps[] = [
     {
       icon: 'LockOpenIcon',
       name: 'Management',
@@ -53,13 +42,12 @@ export default async function SideBar() {
     },
   ]
 
-  if (data?.managementAccess) SidebarProps.elements.push(...ManagementElements)
-
+  if (data?.managementAccess) elements.push(...managementElements)
 
   return (
     <>
-      <MobileSidebar {...SidebarProps} />
-      <DesktopSidebar {...SidebarProps} />
+      <MobileSidebar elements={elements} />
+      <DesktopSidebar elements={elements} />
     </>
   )
 }
