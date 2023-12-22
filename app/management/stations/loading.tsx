76469a732f55672b6b8b715d2d01392c07bc668c
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-12-22 16:35:57 +0100

    feat: added loading animation to /management/stations

---
+++ app/management/stations/loading.tsx
@@ -0,0 +1,29 @@
+import RenderStation from '@/app/(components)/stations/RenderStation'
+import { ObjectId, WithId } from 'mongodb'
+import Station from '@/typings/Station'
+
+export default async function LoadingManagementStations() {
+  const stations = Array.from({ length: 6 }).map(
+    (e): WithId<Station> => ({
+      // @ts-ignore
+      _id: new ObjectId(Math.random()).toString(),
+      name: 'somename',
+      address: {
+        city: '',
+        parkingPlaces: [],
+        coordinates: [],
+      },
+    }),
+  )
+
+  return (
+    <>
+      <h1 className='mb-8 text-center text-3xl font-semibold lg:text-left'>Available Stations</h1>
+      <div className='grid grid-cols-1 gap-4 px-2 xl:grid-cols-2 2xl:grid-cols-2 3xl:grid-cols-3'>
+        {stations.map((s) => (
+          <RenderStation key={Math.random().toString()} station={s} isPending editable />
+        ))}
+      </div>
+    </>
+  )
+}
