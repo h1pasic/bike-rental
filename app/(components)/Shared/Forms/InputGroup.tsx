d6f76f518e840664761bc516b6b535ee4ae87c78
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-12-22 16:12:33 +0100

    feat: updated InputGroup component to use DynamicText
    Updated the InputGroup component to render a flexbox with two DynamicText elements while the component isPending.

--- app/(components)/Shared/Forms/InputGroup.tsx
+++ app/(components)/Shared/Forms/InputGroup.tsx
@@ -1,13 +1,25 @@
 import { twMerge } from 'tailwind-merge'
 import { DetailedHTMLProps, InputHTMLAttributes } from 'react'
+import { DynamicText, ResponsiveElementProps } from '@/app/(components)/Shared/Responsive/DynamicText'
 
-export interface InputGroupProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+export interface InputGroupProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, ResponsiveElementProps {
   labelClassName?: string
 }
 
 export default function InputGroup(props: InputGroupProps) {
   let id = props.id
   if (!id) id = [props?.defaultValue?.toString() ?? '', props?.name?.toString() ?? ''].join('')
+
+  if (props.isPending) {
+    return (
+      <div className='flex items-center gap-2'>
+        <DynamicText content='' skWidth='w-16' isPending />
+        <div className='flex flex-1 rounded-md dark:bg-neutral-600/60'>
+          <DynamicText content='' skWidth='w-full' skContainerClassName='flex-1 py-2 px-2' isPending />
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className='flex items-center gap-2'>
